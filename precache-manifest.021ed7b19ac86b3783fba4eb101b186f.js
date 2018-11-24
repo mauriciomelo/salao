ab30263fb11e494169a8752e71c9cf9b17c2cd5a
Mauricio Melo <mauricioltw@gmail.com>
2018-11-24 14:22:21 -0300

    Updates

---
+++ precache-manifest.021ed7b19ac86b3783fba4eb101b186f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41a61cc6fb35164404c2",
+    "url": "/salao/static/css/main.0c40b667.chunk.css"
+  },
+  {
+    "revision": "41a61cc6fb35164404c2",
+    "url": "/salao/static/js/main.41a61cc6.chunk.js"
+  },
+  {
+    "revision": "2bf373351d086968eb44",
+    "url": "/salao/static/js/1.2bf37335.chunk.js"
+  },
+  {
+    "revision": "b4c50be1b67e0fa23fd2",
+    "url": "/salao/static/js/runtime~main.b4c50be1.js"
+  },
+  {
+    "revision": "4286c84cb7b842e881e1b51b4767a215",
+    "url": "/salao/index.html"
+  }
+];
