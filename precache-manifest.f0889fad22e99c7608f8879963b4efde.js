1420716615af9fd0f872ef8a7f4cc0177db569b9
Mauricio Melo <mauricioltw@gmail.com>
2019-07-24 13:03:27 -0300

    Updates

---
+++ precache-manifest.f0889fad22e99c7608f8879963b4efde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28862baa8b5c1eb442550ded9639fbaa",
+    "url": "/salao/index.html"
+  },
+  {
+    "revision": "3bf179ff9cc8ddfa6b8e",
+    "url": "/salao/static/css/main.b621a43d.chunk.css"
+  },
+  {
+    "revision": "0404fae4d7de49a2b662",
+    "url": "/salao/static/js/2.663177b5.chunk.js"
+  },
+  {
+    "revision": "3bf179ff9cc8ddfa6b8e",
+    "url": "/salao/static/js/main.eb3f4cd6.chunk.js"
+  },
+  {
+    "revision": "d815f14e1c2818f8a904",
+    "url": "/salao/static/js/runtime~main.b3147660.js"
+  }
+]);
