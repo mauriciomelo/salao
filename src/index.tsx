c158059275eca7a6af35e1c6732c5550359caa4a
Mauricio Melo <mauricioltw@gmail.com>
2019-07-25 11:08:04 -0300

    Add redux dev tools

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk'
-import rootReducer from  './rootReducer';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from './rootReducer';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const middlewareEnhancer = applyMiddleware(thunkMiddleware)
+const middlewareEnhancer = applyMiddleware(thunkMiddleware);
 
-const store = createStore(rootReducer, undefined, middlewareEnhancer);
+const store = createStore(rootReducer, composeWithDevTools(middlewareEnhancer));
 
 ReactDOM.render(
   <Provider store={store}>
